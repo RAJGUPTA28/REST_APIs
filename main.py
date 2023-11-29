5751545c474381922db064bd165d955c433b4b1a
Raj Gupta <89084549+RAJGUPTA28@users.noreply.github.com>
2023-11-29 10:12:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,16 @@ import requests
 requests.get("https://randomuser.me/api/")
 # <Response [200]>
 
+
+#GET
+api_url = "https://jsonplaceholder.typicode.com/todos/1"
+response = requests.get(api_url)
+response.json()
+
+#POST
+api_url = "https://jsonplaceholder.typicode.com/todos"
+todo = {"userId": 1, "title": "Buy milk", "completed": False}
+response = requests.post(api_url, json=todo)
+response.json()
+
+
