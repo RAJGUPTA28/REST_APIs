85d2452d9baec7a741d5d5403b2bd47b9c0111f1
Raj Gupta <89084549+RAJGUPTA28@users.noreply.github.com>
2023-11-29 10:15:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,10 +19,15 @@ response.json()
 api_url = "https://jsonplaceholder.typicode.com/todos/10"
 response = requests.get(api_url)
 response.json()
-
 #{'userId': 1, 'id': 10, 'title': 'illo est ... aut', 'completed': True}
 
+#PATCH
 todo = {"userId": 1, "title": "Wash car", "completed": True}
 response = requests.put(api_url, json=todo)
 response.json()
 
+api_url = "https://jsonplaceholder.typicode.com/todos/10"
+todo = {"title": "Mow lawn"}
+response = requests.patch(api_url, json=todo)
+response.json()
+#{'userId': 1, 'id': 10, 'title': 'Mow lawn', 'completed': True}
